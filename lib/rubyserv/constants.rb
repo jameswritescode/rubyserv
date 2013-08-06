68d33c5c7d16471b0ffcecaf46f9cda06109e5a3
James Newton <james@Zaphyous.com>
2013-08-05 20:43:37 -0400

    strip VERSION

--- lib/rubyserv/constants.rb
+++ lib/rubyserv/constants.rb
@@ -1,4 +1,4 @@
 module RubyServ
-  VERSION  = File.read(RubyServ.root.join('VERSION'))
+  VERSION  = File.read(RubyServ.root.join('VERSION')).strip
   REVISION = `git log --pretty=format:'%h' -n 1`
 end
