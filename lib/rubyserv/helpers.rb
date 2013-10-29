921068a1a3fcb6d61ce9c38248995c0fd4a9fb46
James Newton <james@Zaphyous.com>
2013-10-29 02:49:29 -0500

    add format_number helper

--- lib/rubyserv/helpers.rb
+++ lib/rubyserv/helpers.rb
@@ -1,5 +1,11 @@
 module RubyServ
-  def self.root
+  extend self
+
+  def root
     Pathname.new(File.dirname(__FILE__) + '/../..').expand_path
+  end
+
+  def format_number(number)
+    number < 1000 ? number : number.to_s.to_str.split('.')[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, '\\1,')
   end
 end
